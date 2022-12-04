66f22b17b22517230bbbe608bef4d86335f5235f
mcisne4 <mcisne4@gmail.com>
2022-12-03 21:03:11 -0600

    Add SvgMoon and SvgSun

--- src/lib/svg/index.ts
+++ src/lib/svg/index.ts
@@ -3,5 +3,7 @@ import SvgCode from './SvgCode.svelte';
 import SvgCodeEdit from './SvgCodeEdit.svelte';
 import SvgCodePage from './SvgCodePage.svelte';
 import SvgHome from './SvgHome.svelte';
+import SvgMoon from './SvgMoon.svelte';
+import SvgSun from './SvgSun.svelte';
 
-export { SvgCarrot, SvgCode, SvgCodeEdit, SvgCodePage, SvgHome };
+export { SvgCarrot, SvgCode, SvgCodeEdit, SvgCodePage, SvgHome, SvgMoon, SvgSun };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import SvgSun from '$lib/svg/SvgSun.svelte';
+	import SvgMoon from '$lib/svg/SvgMoon.svelte';
+</script>
+
+<SvgSun />
+<SvgMoon />
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
